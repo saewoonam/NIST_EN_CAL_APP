d40810318d2b978ad1d768dcc3eb359bb4f16fdd
Sae Woo Nam <nams@nist.gov>
2020-09-21 12:10:10 -0600

    More cleanup of non-sensical references to other code

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,13 @@
-// (c) 2014 Don Coleman
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Based on examples written by Don Coleman as part of his 
+// cordova-central-ble library
 
+// Somehow a bunch of stuff is global, not sure how this is done...
+//
 /* global mainPage, deviceList, refreshButton */
 /* global detailPage, resultDiv, messageInput, sendButton, disconnectButton */
 /* global ble  */
 /* jshint browser: true , devel: true*/
+
 'use strict';
 
 // ASCII only
@@ -100,7 +92,7 @@ var app = {
         };
 
         var failure = function() {
-            alert("Failed writing data to the nisten_ble le");
+            alert("Failed writing data to the nisten_ble");
         };
 
         var data = stringToBytes(messageInput.value);
